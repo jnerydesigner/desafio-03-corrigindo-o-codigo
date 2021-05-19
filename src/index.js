e53e23d30581d8fea6cc3df9e9248bd276570340
jnerydesigner <jander.webmaster@gmail.com>
2021-05-19 13:58:29 -0400

    Add challenges rocketseat

--- src/index.js
+++ src/index.js
@@ -23,20 +23,28 @@ app.post("/repositories", (request, response) => {
     likes: 0
   };
 
-  return response.json(repository);
+  repositories.push(repository);
+
+  return response.status(201).json(repository);
 });
 
 app.put("/repositories/:id", (request, response) => {
   const { id } = request.params;
   const updatedRepository = request.body;
 
-  repositoryIndex = repositories.findindex(repository => repository.id === id);
+
+
+  repositoryIndex = repositories.findIndex(repository => repository.id === id);
 
   if (repositoryIndex < 0) {
     return response.status(404).json({ error: "Repository not found" });
   }
 
-  const repository = { ...repositories[repositoryIndex], ...updatedRepository };
+  if (updatedRepository.likes > 0) {
+    updatedRepository.likes = repositories[repositoryIndex].likes;
+  }
+
+  const repository = { ...repositories[repositoryIndex], ...updatedRepository, ...repositories[repositoryIndex].likes };
 
   repositories[repositoryIndex] = repository;
 
@@ -52,13 +60,16 @@ app.delete("/repositories/:id", (request, response) => {
     return response.status(404).json({ error: "Repository not found" });
   }
 
-  repositories.splice(repositoryIndex, 1);
+  repositories.splice(repositories[repositoryIndex]);
 
+  // aqui
   return response.status(204).send();
+
 });
 
 app.post("/repositories/:id/like", (request, response) => {
   const { id } = request.params;
+
 
   repositoryIndex = repositories.findIndex(repository => repository.id === id);
 
@@ -68,7 +79,7 @@ app.post("/repositories/:id/like", (request, response) => {
 
   const likes = ++repositories[repositoryIndex].likes;
 
-  return response.json('likes');
+  return response.json(repositories[repositoryIndex]);
 });
 
 module.exports = app;
